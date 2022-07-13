d446c948c33615b9ee52ed1cc075fea2f350ab0f
rere950303 <yhwjjang1995@naver.com>
2022-07-13 10:25:16 +0900

    refactor: add null check logic

--- src/main/java/com/github/rere950303/apiutil/exception/ApiResponseException.java
+++ src/main/java/com/github/rere950303/apiutil/exception/ApiResponseException.java
@@ -2,29 +2,36 @@ package com.github.rere950303.apiutil.exception;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+
+import java.util.Objects;
 
 @Getter
 public class ApiResponseException extends RuntimeException {
 
-    private HttpStatus status = HttpStatus.BAD_REQUEST;
+    private HttpStatus status;
 
-    public ApiResponseException(String message, HttpStatus status) {
+    public ApiResponseException(String message, @NonNull HttpStatus status) {
         super(message);
+        Objects.requireNonNull(status);
         this.status = status;
     }
 
-    public ApiResponseException(HttpStatus status) {
+    public ApiResponseException(@NonNull HttpStatus status) {
         super();
+        Objects.requireNonNull(status);
         this.status = status;
     }
 
-    public ApiResponseException(String message, Throwable cause, HttpStatus status) {
+    public ApiResponseException(String message, Throwable cause, @NonNull HttpStatus status) {
         super(message, cause);
+        Objects.requireNonNull(status);
         this.status = status;
     }
 
-    public ApiResponseException(Throwable cause, HttpStatus status) {
+    public ApiResponseException(Throwable cause, @NonNull HttpStatus status) {
         super(cause);
+        Objects.requireNonNull(status);
         this.status = status;
     }
 }
